e7e4506299a05b0712296a7f42f9684acb8641cd
skysea <arcade0425@gmail.com>
2023-12-17 17:45:34 +0800

    DEL: Delete Taipei Youbike url

--- app/constants.py
+++ app/constants.py
@@ -1,5 +1,4 @@
 XINBEI_YOUBIKE_URL = 'https://data.ntpc.gov.tw/api/datasets/71cd1490-a2df-4198-bef1-318479775e8a/json'
 XINBEI_YOUBIKE2_URL = 'https://data.ntpc.gov.tw/api/datasets/010e5b15-3823-4b20-b401-b1cf000550c5/json'
 
-# TAIPEI_YOUBIKE_URL = 'https://tcgbusfs.blob.core.windows.net/blobyoubike/YouBikeTP.json'
 TAIPEI_YOUBIKE2_URL = 'https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v2/youbike_immediate.json'
